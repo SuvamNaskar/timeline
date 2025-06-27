611392f46b7773f8b3a392c35170d9d3ede1c30e
Suvam Naskar <suvamnaskar780@gmail.com>
2025-06-27 11:58:37 +0530

    Update index view context with simplified title and user information

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
     context = {
-        'title': 'Home Page',
+        'title': 'Home',
+        'current_year': datetime.now().year,
+        'user_name': 'your_username',
+        'user_display_name': 'Your Name',
     }
 
     return render(request, 'index.html', context)
